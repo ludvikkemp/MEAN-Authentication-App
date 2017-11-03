6fb59f078ffd3e8ea8ba40245148390a5fd44c2c
ludvikkemp <ludvik11@ru.is>
2017-11-03 06:41:49 +0000

    Backend for adding user to mongodb and authenticating routes ready. Also first commit :)

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const config = require('./config/database');
+
+// Connect to Database
+mongoose.connect(config.database, { useMongoClient: true });
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Database error' + err);
+});
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+// Route /users to users.js file
+app.use('/users', users);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+app.listen(port, () => {
+  console.log('Server started on port' + port);
+});

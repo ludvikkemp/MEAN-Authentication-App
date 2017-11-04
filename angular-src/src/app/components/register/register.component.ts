93d55b412c6f659d43afd8169da8e6ca8742d8d9
ludvikkemp <ludvik11@ru.is>
2017-11-04 07:49:58 +0000

    auth.services added, now it's possible to register a new user to backend but same username are still allowed and needs fixing

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +15,12 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService) { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -25,12 +33,27 @@ export class RegisterComponent implements OnInit {
       password: this.password
     }
 
+    // Validate
     if (!this.validateService.validateRegister(user)) {
+      this.flashMessage.show('Please fill in all fields', { cssClass: 'alert-danger', timeout: 3000 });
       return false;
     }
     if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessage.show('Please enter a valid email', { cssClass: 'alert-danger', timeout: 3000 });
       return false;
     }
+
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessage.show('You have Successfully Registered', { cssClass: 'alert-success', timeout: 3000 });
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('You have Successfully Registered', { cssClass: 'alert-danger', timeout: 3000 });
+        this.router.navigate(['/regiser']);
+      }
+    });
+
   }
 
 
